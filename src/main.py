800a9ba2be0822aecf9b1f811154acd0670b3a0b
BenjaminHRC <ben430hoarau@gmail.com>
2025-04-02 18:55:20 +0200

    feat : add metric model prophet

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
+from prophet.diagnostics import performance_metrics, cross_validation
 import pickle
 
 app = FastAPI()
@@ -14,3 +15,8 @@ def predict(days: int = 30):
     future = model.make_future_dataframe(periods=days, freq='D')
     forecast = model.predict(future)
     return forecast[['ds', 'yhat']].tail(days).to_dict(orient="records")
+
+@app.get("/metrics/prophet")
+def metrics_prophet():
+    df_cv = cross_validation(model, initial='2190 days', period='30 days', horizon = "365 days")
+    return performance_metrics(df_cv).to_dict(orient="records")
