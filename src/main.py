cca3a7071871cce8bcdc4a333380be79ff5633fa
BenjaminHRC <ben430hoarau@gmail.com>
2025-04-23 10:16:33 +0200

    wip

--- src/main.py
+++ src/main.py
@@ -1,22 +1,63 @@
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
-from prophet.diagnostics import performance_metrics, cross_validation
+from prometheus_client import Histogram, Gauge
+from model_prophet import get_test_data, train
+from sklearn.metrics import r2_score, mean_absolute_percentage_error, mean_squared_error
 import pickle
+import time
+
 
 app = FastAPI()
 Instrumentator().instrument(app).expose(app)
 
+# Histogramme pour mesurer le temps de réponse de /predict
+PREDICT_RESPONSE_TIME = Histogram(
+    "predict_response_time_seconds",
+    "Temps de réponse de l'endpoint /predict",
+    buckets=[0.1, 0.5, 1, 2, 5, 10, 30]
+)
+
+# Gauges pour stocker les métriques de performance du modèle
+R2_SCORE = Gauge("model_r2_score", "Coefficient de détermination du modèle")
+RMSE = Gauge("model_rmse", "Root Mean Squared Error du modèle")
+MAPE = Gauge("model_mape", "Mean Absolute Percentage Error du modèle")
+
+train()
+
 # Charger le modèle Prophet
 with open("./models/prophet_model.pkl", "rb") as f:
     model = pickle.load(f)
+    
+# Charger les données de test pour évaluer le modèle
+df_test = get_test_data()
+df_test.rename(columns={"Date": "ds", "Consommation": "y"}, inplace=True)
 
 @app.get("/predict/")
 def predict(days: int = 30):
+    start_time = time.time()
     future = model.make_future_dataframe(periods=days, freq='D')
     forecast = model.predict(future)
+    duration = time.time() - start_time
+    
+    PREDICT_RESPONSE_TIME.observe(duration)
+    
     return forecast[['ds', 'yhat']].tail(days).to_dict(orient="records")
 
-@app.get("/metrics/prophet")
-def metrics_prophet():
-    df_cv = cross_validation(model, initial='2190 days', period='30 days', horizon = "365 days")
-    return performance_metrics(df_cv).to_dict(orient="records")
+@app.get("/metrics/model")
+def compute_model_metrics():
+    """ Calcule les métriques de performance du modèle """
+    future = model.predict(df_test[['ds']])
+    y_true = df_test['y'].values
+    y_pred = future['yhat'].values
+
+    # Calcul des métriques
+    r2 = r2_score(y_true, y_pred)
+    rmse = mean_squared_error(y_true, y_pred) ** 0.5
+    mape = mean_absolute_percentage_error(y_true, y_pred)
+
+    # Mettre à jour les métriques Prometheus
+    R2_SCORE.set(r2)
+    RMSE.set(rmse)
+    MAPE.set(mape)
+    
+    return {"r2_score": r2, "rmse": rmse, "mape": mape}
--- src/model_prophet.py
+++ src/model_prophet.py
@@ -6,44 +6,75 @@ import pickle
 from os import listdir
 from os.path import isfile, join
 
+array_dfs = None
+df_final = None
+df_data_train = None
+df_data_test = None
+df_train_prophet = None
+model_prophet = None
 
-""" Extract datasource """
-sources = [f for f in listdir("./datasets") if isfile(join("./datasets", f))]
+def train():
+    extract_datasource()
+    transform_dataframe()
+    prepare_data()
+    train_model()
+    save_model()
 
-array_dfs = []
+def extract_datasource():
+    """ Extract datasource """
+    global array_dfs
+    sources = [f for f in listdir("./datasets") if isfile(join("./datasets", f))]
 
-for source in sources:
-        if source != '.gitkeep':
-            array_dfs.append(pd.read_csv(f"./datasets/{source}", low_memory=False))
+    array_dfs = []
 
-""" Transform Dataframe """
-df = pd.concat(array_dfs, ignore_index=True, sort=False)
+    for source in sources:
+            if source != '.gitkeep':
+                array_dfs.append(pd.read_csv(f"./datasets/{source}", low_memory=False))
 
-df_filtered = df[['Date', 'Consommation']]
+def transform_dataframe():
+    """ Transform Dataframe """
+    global df_final
+    df = pd.concat(array_dfs, ignore_index=True, sort=False)
 
-df_not_nan_consommation = df_filtered[df_filtered['Consommation'].notna()]
+    df_filtered = df[['Date', 'Consommation']]
 
-df_sorted = df_not_nan_consommation.sort_values(by='Date')
+    df_not_nan_consommation = df_filtered[df_filtered['Consommation'].notna()]
 
-# aggregation calc mean of Consommation / day
-df_final = df_sorted.groupby('Date', as_index=False).agg({'Consommation': 'mean'})
+    df_sorted = df_not_nan_consommation.sort_values(by='Date')
 
-""" Prepare data """
-df_data_train = df_final[df_final['Date'] < "2020"]
-df_data_test = df_final[df_final['Date'] >= "2020"]
+    # aggregation calc mean of Consommation / day
+    df_final = df_sorted.groupby('Date', as_index=False).agg({'Consommation': 'mean'})
 
-df_train_prophet = df_data_train
+def prepare_data():
+    """ Prepare data """
+    global df_data_train
+    global df_data_test
+    global df_train_prophet
+    
+    df_data_train = df_final[df_final['Date'] < "2020"]
+    df_data_test = df_final[df_final['Date'] >= "2020"]
 
-# date variable needs to be named "ds" for prophet
-df_train_prophet = df_train_prophet.rename(columns={"Date": "ds"})
+    df_train_prophet = df_data_train
 
-# target variable needs to be named "y" for prophet
-df_train_prophet = df_train_prophet.rename(columns={"Consommation": "y"})
+    # date variable needs to be named "ds" for prophet
+    df_train_prophet = df_train_prophet.rename(columns={"Date": "ds"})
 
-""" Train model """
-model_prophet = Prophet()
-model_prophet.fit(df_train_prophet)
+    # target variable needs to be named "y" for prophet
+    df_train_prophet = df_train_prophet.rename(columns={"Consommation": "y"})
 
-""" Save model """
-with open("./models/prophet_model.pkl", "wb") as f:
-    pickle.dump(model_prophet, f)
+def train_model():
+    """ Train model """
+    global model_prophet
+    model_prophet = Prophet()
+    model_prophet.fit(df_train_prophet)
+    
+def save_model():
+    """ Save model """
+    with open("./models/prophet_model.pkl", "wb") as f:
+        pickle.dump(model_prophet, f)
+
+def get_train_data():
+    return df_data_train
+
+def get_test_data():
+    return df_data_test
